900813d6b713a4d4aa44685190a78037d302f75e
maliknur <malik.dba.pro@gmail.com>
2016-05-28 00:06:49 -0700

    change app style

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import NotesEditor from './NotesEditor.jsx';
 import NotesGrid from './NotesGrid.jsx';
 
+import './App.less';
+
 const App = React.createClass({
 	handNoteAdd(data) {
 		console.log(data);
@@ -10,7 +12,7 @@ const App = React.createClass({
 	render() {
 		return (
 				<div className="App">
-					<h2 className="App__header">Reactive Notes App</h2>
+					<h2 className="App__header"><strong>Reactive</strong> Notes App</h2>
 					<NotesEditor onNoteAdd = {this.handNoteAdd} />
 					<NotesGrid />
 
