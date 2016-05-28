36caa6f9bf2d78b16f14460f21ce88671a0ffe38
maliknur <malik.dba.pro@gmail.com>
2016-05-28 00:30:49 -0700

    fix flux actions

--- client/actions/NotesActions.js
+++ client/actions/NotesActions.js
@@ -4,44 +4,45 @@ import Constants from '../constants/AppConstants';
 import api from '../api';
 
 const NoteActions = {
-	loadNotes() {
-		AppDispatcher.dispatch({
-			type: Constants.LOAD_NOTES_REQUEST
-		});
+    loadNotes() {
+        AppDispatcher.dispatch({
+            type: Constants.LOAD_NOTES_REQUEST
+        });
 
-		api.listNotes()
-			.then(({ data }) => 
-				AppDispatcher.dispatch({
-					type: Constants.LOAD_NOTES_SUCCESS,
-					notes: data
-				})
-			)
-			.catch( err => 
+        api.listNotes()
+        .then(({ data }) =>
+            AppDispatcher.dispatch({
+                type: Constants.LOAD_NOTES_SUCCESS,
+                notes: data
+            })
+        )
+        .catch(err =>
+            AppDispatcher.dispatch({
+                type: Constants.LOAD_NOTES_FAIL,
+                error: err
+            })
+        );
+    },
 
-				AppDispatcher.dispatch({
-					type: Constants.LOAD_NOTES_FAIL,
-					error: err
-				})
-			);
-	},
+    createNote(note) {
+        api.createNote(note)
+        .then(() =>
+            this.loadNotes()
+        )
+        .catch(err =>
+            console.error(err)
+        );
+    },
 
-	createNote(note) {
-		api.createNote(note)
-			.then(() => 
-				this.loadNotes()
-			)
-			.catch(err => 
-				console.error(err)
-			);
-	},
+    deleteNote(noteId) {
+        api.deleteNote(noteId)
+        .then(() =>
+            this.loadNotes()
+        )
+        .catch(err =>
+            console.error(err)
+        );
+    }
+};
 
-	deleteNotes(noteId) {
-		api.deleteNote(noteId)
-			.then(() => 
-				this.loadNotes()
-			)
-			.catch(err =>
-				console.error(err)
-			);
-	}
-}
+export default NoteActions;
